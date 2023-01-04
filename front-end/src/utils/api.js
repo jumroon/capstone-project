78ab1924bb53aca62540ccc2bd87043b2b344c0a
jumroon <jumroon.rada@gmail.com>
2023-01-04 03:35:55 -0600

    formatted reservation Date and Time on mobile search page

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -61,7 +61,7 @@ async function fetchJson(url, options, onCancel) {
 export async function listReservations(params, signal) {
   const url = new URL(`${API_BASE_URL}/reservations`);
   Object.entries(params).forEach(([key, value]) =>
-    url.searchParams.append(key, value.toString()),
+    url.searchParams.append(key, value.toString())
   );
   return await fetchJson(url, { headers, signal }, [])
     .then(formatReservationDate)
@@ -143,7 +143,9 @@ export async function searchReservationsByPhoneNumber(mobile_number, signal) {
     headers,
     signal,
   };
-  return await fetchJson(url, options, {});
+  return await fetchJson(url, options, {})
+    .then(formatReservationDate)
+    .then(formatReservationTime);
 }
 
 export async function updateReservation(reservation, reservation_id, signal) {
